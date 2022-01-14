cf546c6a5d801fdd9af439f1ea5d1413ae49ddc8
longlong <562172151@qq.com>
2022-01-14 15:19:44 +0800

    feat :  add dest page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const myplugin = require('./myplugin')
 module.exports = {
     title: '',
     base:'/loong-totem-view/',
-    dest:'./docs/doc',
+    dest:'page',
     theme: path.resolve(__dirname, 'theme/index.js'),
     markdown: {
         importCode: {
