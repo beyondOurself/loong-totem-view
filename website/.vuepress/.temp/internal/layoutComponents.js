beda87a3a1786854130cda4a304db6c8c61f4132
longlong <562172151@qq.com>
2022-01-20 19:12:09 +0800

    reset git ignore

--- website/.vuepress/.temp/internal/layoutComponents.js
+++
@@ -1,6 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const layoutComponents = {
-  "404": defineAsyncComponent(() => import("C:/Users/Getech-200107-1/Desktop/loong-totem-view/website/.vuepress/theme/layouts/404.vue")),
-  "Layout": defineAsyncComponent(() => import("C:/Users/Getech-200107-1/Desktop/loong-totem-view/website/.vuepress/theme/layouts/Layout.vue")),
-}
--- website/.vuepress/.temp/pages/components/tag.html.vue
+++
@@ -1,3 +0,0 @@
-<template><h1 id="tag" tabindex="-1"><a class="header-anchor" href="#tag" aria-hidden="true">#</a> tag</h1>
-<p>这是tag</p>
-</template>
