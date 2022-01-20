e5d186378b2a02ab5a880ae1045bf9d94db58285
longlong <562172151@qq.com>
2022-01-20 19:04:30 +0800

    feat : git init

--- website/.vuepress/clientAppEnhance.ts
+++
@@ -1,9 +0,0 @@
-import { defineClientAppEnhance } from '@vuepress/client'
-import { path } from '@vuepress/utils'
-import loong from '../../packages/loong-totem-view/index.js'
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-
-    // console.log('siteData', siteData)
-    app.use(loong)
-     
-})
--- website/.vuepress/theme/layouts/404.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>404 看不见</div>
-</template>
