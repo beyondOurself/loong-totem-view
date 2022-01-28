c39951e0fceb7b00a571955180376db0197fb91d
longlong <562172151@qq.com>
2022-01-28 19:32:37 +0800

    docs:  adjust color

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,12 +10,10 @@ module.exports = {
         // 作为特例，默认语言可以使用 '/' 作为其路径。
         '/': {
             lang: 'zh-CN',
-            title: 'VuePress',
             description: 'Vue-powered Static Site Generator',
         },
         '/en/': {
             lang: 'en-US',
-            title: 'VuePress',
             description: 'Vue 驱动的静态网站生成器',
         },
     },
